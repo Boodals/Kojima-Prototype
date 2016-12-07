1c2df6d7193e5646ff9b88afe23b068e948f0bd6
t2-millssmith <Thomas465@live.co.uk>
2016-12-07 23:28:08 +0000

    Added the models from the original game and created some scripts to fill in later

---
+++ Assets/PlayerCameraScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraScript : MonoBehaviour {
+
+    public PlayerScript myPlayer;
+
+    float turnSpeed = 8;
+    float distanceFromPlayer = 4.5f;
+
+    
+    public Vector3 curPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw(myPlayer.horizontal2), Input.GetAxisRaw(myPlayer.vertical2), 0);
+
+        curPos += input * turnSpeed * Time.deltaTime;
+
+        curPos.y = Mathf.Clamp(curPos.y, -60, 60);
+
+
+        transform.position = myPlayer.transform.position + curPos;
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody[] wheels;
+
+    public string horizontal, vertical, horizontal2, vertical2, accelerate, brake, drift, handbrake;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical), 0);
+    }
+}

e6be6571acd6ce3225980762ff44ca0b142820c2
t2-millssmith <Thomas465@live.co.uk>
2016-12-08 00:36:07 +0000

    Had a go with Unity's wheel colliders, very glitchy right now

--- Assets/PlayerCameraScript.cs
+++ Assets/PlayerCameraScript.cs
@@ -5,15 +5,15 @@ public class PlayerCameraScript : MonoBehaviour {
 
     public PlayerScript myPlayer;
 
-    float turnSpeed = 8;
-    float distanceFromPlayer = 4.5f;
-
-    
+    float turnSpeed = 90;
+    float distanceFromPlayer = 7.5f;
+   
     public Vector3 curPos;
 
 	// Use this for initialization
 	void Start () {
-	
+        curPos = -myPlayer.transform.eulerAngles;
+        transform.position = myPlayer.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -23,13 +23,17 @@ public class PlayerCameraScript : MonoBehaviour {
 
     void FixedUpdate()
     {
-        Vector3 input = new Vector3(Input.GetAxisRaw(myPlayer.horizontal2), Input.GetAxisRaw(myPlayer.vertical2), 0);
+        Vector3 input = new Vector3(Input.GetAxisRaw(myPlayer.vertical2), Input.GetAxisRaw(myPlayer.horizontal2), 0);
 
         curPos += input * turnSpeed * Time.deltaTime;
 
-        curPos.y = Mathf.Clamp(curPos.y, -60, 60);
+        curPos.x = Mathf.Clamp(curPos.x, 10, 60);
 
+        Vector3 backwards = new Vector3(0, 0, -distanceFromPlayer);
+        Quaternion rot = Quaternion.Euler(curPos);
 
-        transform.position = myPlayer.transform.position + curPos;
+        transform.position = Vector3.Lerp(transform.position, myPlayer.transform.position + (rot * backwards), 8 * Time.deltaTime);
+
+        transform.LookAt(myPlayer.transform.position);
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    public Rigidbody[] wheels;
+    public WheelCollider[] wheels;
+    public Transform[] visualWheels;
 
     public string horizontal, vertical, horizontal2, vertical2, accelerate, brake, drift, handbrake;
 
@@ -21,5 +22,28 @@ public class PlayerScript : MonoBehaviour {
     void FixedUpdate()
     {
         Vector3 input = new Vector3(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical), 0);
+
+        //Rotate front wheels
+        for (int i = 2; i < 4; i++)
+        {
+            wheels[i].steerAngle = Mathf.Lerp(wheels[i].steerAngle, 45 * input.x, 4 * Time.deltaTime);
+        }
+
+        //Accelerate using back wheels
+        for (int i = 0; i < 2; i++)
+        {
+            wheels[i].motorTorque = 1000 * Input.GetAxisRaw(accelerate);
+        }
+
+        for (int i=0; i<wheels.Length; i++)
+        {
+            Vector3 pos;
+            Quaternion rot;
+
+            wheels[i].GetWorldPose(out pos, out rot);
+
+            visualWheels[i].transform.position = pos;
+            visualWheels[i].transform.rotation = rot;
+        }
     }
 }

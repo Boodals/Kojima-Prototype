ff757793643d4c6f2b55f43b6e719810ab2ffdbc
UWETom123 <tom.moreton@yahoo.co.uk>
2016-12-10 00:37:30 +0000

    Trap spawn script working however rotation of spawned trap is a bit bodgejob

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.rotation = new Quaternion(-90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/TrapSpawner.cs
+++ Assets/Scripts/TrapSpawner.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class TrapSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public bool spawnTraps;
+    public float spawnInterval;
+    public float currentTimer = 0.0f;    
+    public GameObject spawnObject;
+
+    //Quaternion trapRotation;
+    Object[] traps;
+
+    // Use this for initialization
+    void Start () {
+        traps = Resources.LoadAll("Traps");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (spawnTraps == true)
+        {
+            currentTimer += Time.deltaTime;
+
+            if (currentTimer >= spawnInterval)
+            {
+                currentTimer = 0.0f;
+                //trapRotation = new Quaternion(Quaternion.identity.x, Quaternion.identity.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+                Instantiate(traps[Random.Range(0, traps.Length)], spawnObject.transform.position, Quaternion.identity);
+            }
+        }
 	}
 }

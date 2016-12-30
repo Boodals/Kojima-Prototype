ed2f3c1aa9d7714d16d630dffc1c04d4d21d8fd0
c3mundy <chantelle2.mundy@live.uwe.ac.uk>
2016-12-30 13:36:23 +0000

    Coded the start game countdown on the HUD with graphics

--- Assets/Scripts/PlayerHUDScript.cs
+++ Assets/Scripts/PlayerHUDScript.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class PlayerHUDScript : MonoBehaviour {
+public class PlayerHUDScript : MonoBehaviour
+{
+    //COUNTDOWN VARIABLES
+    public TrafficLightScript[] myLights;
+    public Text countdownTxt;
+
+    float countdownTimer;
+    int countdownNum = 3;
+    Color startColor;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        countdownTxt.text = "3";
+        startColor = countdownTxt.color;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        countdownTimer -= Time.deltaTime;
+
+        countdownTxt.transform.localScale = Vector3.Lerp(countdownTxt.transform.localScale, Vector3.zero, Time.deltaTime);
+
+        if(countdownTimer <= 0)
+        {
+            Countdown();
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void Countdown()
+    {
+        countdownTxt.transform.localScale = Vector3.one;
+
+        for (int i = 0; i < 2; i++)
+        {
+            myLights[i].ChangeColour(countdownNum);
+        }
+
+        if (countdownNum > 0)
+        {         
+            countdownTxt.color = startColor;
+            countdownTxt.text = "" + countdownNum;
+            countdownNum--;
+            countdownTimer = 2;            
+        }
+        else
+        {
+            countdownTxt.text = "GO";
+            countdownTxt.color = Color.Lerp(countdownTxt.color, Color.clear, Time.deltaTime * 2);
+        }
+    }
 }
---
+++ Assets/Scripts/TrafficLightScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrafficLightScript : MonoBehaviour
+{
+    public Image[] glowColours;
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    bool showLights;
+    float delayTimer = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        showLights = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(showLights)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, startPos, Time.deltaTime * 5);
+        }
+        else
+        {
+            delayTimer -= Time.deltaTime;
+
+            if (delayTimer <= 0)
+            {
+                transform.localPosition = Vector3.Lerp(transform.localPosition, endPos, Time.deltaTime * 2);
+            }
+        }
+	}
+
+    public void ChangeColour(int timeStage)
+    {
+        switch(timeStage)
+        {
+            case 0:
+                glowColours[1].enabled = false;
+                glowColours[0].enabled = true;
+                MoveLights(1);
+                break;
+            case 1:
+                glowColours[2].enabled = false;
+                glowColours[1].enabled = true;
+                break;
+            case 2:
+                glowColours[2].enabled = true;
+                break;
+            case 3:
+                glowColours[2].enabled = true;
+                MoveLights(0);
+                break;
+        }
+    }
+
+    void MoveLights(int direction)
+    {
+        if(direction == 0)
+        {
+            showLights = true;
+        }
+        else
+        {
+            showLights = false;
+        }
+    }
+}

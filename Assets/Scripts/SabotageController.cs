4823a1c6d9a0662d88dbba2aee045deb33681d65
OCHyams <Orlando2.Cazalet-Hyams@uwe.live.ac.uk>
2017-01-14 22:51:17 +0000

    Working on sabotage mechanics

--- Assets/Scripts/SabotageController.cs
+++ Assets/Scripts/SabotageController.cs
@@ -7,7 +7,8 @@ public class SabotageController : MonoBehaviour
     public int ChaseCar { get; set; }
     public static SabotageController Singleton { get { return m_singleton; } }
     private static SabotageController m_singleton;
-   
+    public GameObject m_projectorObject;
+    public bool m_firstFrame = true;
 
     protected void Start()
     {
@@ -24,16 +25,23 @@ public class SabotageController : MonoBehaviour
         screenSetup.camInfos[0].viewStyle = PlayerCameraScript.ViewStyles.Overhead;
         screenSetup.camInfos[0].positionOnScreen = PlayerCameraScript.ScreenPositions.FullScreen;
         CameraManagerScript.singleton.NewScreenSetup(screenSetup);
-
-
-        /*Add off-screen death script to the ChaseCar*/
-        GameController.singleton.players[ChaseCar].gameObject.AddComponent<KillIfOffscreen>();
+        
     }
 
-
-
-    protected void LateUpdate()
+    protected void Update()
     {
-     
+        /*Hacky work around setup ordering*/
+        if (m_firstFrame)
+        {
+            Camera ref_mainCam = CameraManagerScript.singleton.playerCameras[0].Cam;
+            m_projectorObject.transform.SetParent(ref_mainCam.transform);
+            /*Attach the projector to the overhead camera*/
+            m_projectorObject.transform.localRotation = Quaternion.identity;
+            m_projectorObject.transform.localPosition = Vector3.zero;
+
+
+            /*Add off-screen death script to the ChaseCar*/
+            GameController.singleton.players[ChaseCar].gameObject.AddComponent<KillIfOffscreen>();
+        }
     }
 }
